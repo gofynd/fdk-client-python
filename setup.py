8e60ffc12e7aa1714659f474b8cfe1cf8023773d
Jigar Dafda <jigar.dafda@gmail.com>
2025-03-28 18:26:37 +0000

    [Auto Generated] 0.0.1111-3.beta.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='fdk_client',
-    version='0.0.1111-1.beta.2',
+    version='0.0.1111-3.beta.1',
     author='Manish Magnani',
     description=description,
     long_description=long_description,
